a9ba7131de26907075f4a20fb62958e94bc0fd85
natiox <nalter@gmail.com>
2024-07-19 17:04:20 -0600

    Finished chapter 10

--- chapter-10/before/script.js
+++ chapter-10/before/script.js
@@ -1,24 +1,25 @@
 
 /** On Load */
 
-(function() {
-  console.log('load');
-  const fields = getAllFieldsAsArray();
-  //  add blur listener to mark it as having been touched by the user
-  fields.forEach(input => input.addEventListener('blur', markAsDirty));
-})()
+//Some of these weren't needed with the new :user-invalid pseudo-class
+// (function() {
+//   console.log('load');
+//   const fields = getAllFieldsAsArray();
+//   //  add blur listener to mark it as having been touched by the user
+//   fields.forEach(input => input.addEventListener('blur', markAsDirty));
+// })()
 
-function getAllFieldsAsArray() {
-  const fields = [ 'input', 'textarea', 'select' ];
-  //  get arrays of each field types
-  return fields.map(fieldType => Array.from(document.querySelectorAll(fieldType)))
-  //  flatten the array
-  .flat();
-}
+// function getAllFieldsAsArray() {
+//   const fields = [ 'input', 'textarea', 'select' ];
+//   //  get arrays of each field types
+//   return fields.map(fieldType => Array.from(document.querySelectorAll(fieldType)))
+//   //  flatten the array
+//   .flat();
+// }
 
-function markAsDirty(event) {
-  event.target.classList.add('dirty');
-}
+// function markAsDirty(event) {
+//   event.target.classList.add('dirty');
+// }
 
 function send(event) { 
   console.log('send');
